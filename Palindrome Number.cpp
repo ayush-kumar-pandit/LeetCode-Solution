fdf12a35349157d68a051905c4691976677c4820
Ayush <ayushpandit672@gmail.com>
2025-10-02 20:45:46 +0530

    Palindrome Number LeetCode

---
+++ Palindrome Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        int temp = x;
+        long y = 0;
+        while(temp){
+            y *= 10;
+            y += temp % 10;
+            temp /= 10;
+        }
+        return x == y;
+    }
+};
