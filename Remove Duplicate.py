bde74748e79aa85a47f7d4500d535633852258b7
Ayush <ayushpandit672@gmail.com>
2025-10-14 11:02:08 +0530

    Remove Duplicate from Sorted array

--- Remove Duplicate.py
+++ Remove Duplicate.py
@@ -5,7 +5,7 @@ class Solution:
 
         i = 0 
 
-        for j in range(1, len(nums)):
+        for j in range(len(nums)):
             if nums[j] != nums[i]:
                 i += 1
                 nums[i] = nums[j]
