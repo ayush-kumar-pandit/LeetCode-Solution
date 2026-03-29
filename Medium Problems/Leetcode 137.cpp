89956917129dcf3844fef467799d437c6c6ede1b
Ayush <ayushpandit672@gmail.com>
2026-03-29 06:10:02 +0530

    Single Number II

---
+++ Medium Problems/Leetcode 137.cpp
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dic = {}
+        for i in nums:
+            if i not in dic:
+                dic[i] = 1
+            else:
+                dic[i] += 1
+
+        for i in nums:
+            if dic[i] == 1:
+                return i
+            
