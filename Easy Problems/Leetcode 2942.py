864e4b3c7be7cbc00312606faa1bb35b823ab5a5
Ayush <ayushpandit672@gmail.com>
2026-04-03 23:17:06 +0530

    Find Words Containing Character

---
+++ Easy Problems/Leetcode 2942.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        ls = []
+        for i in range(len(words)):
+            if x in words[i]:
+                ls.append(i)
+
+        return ls
