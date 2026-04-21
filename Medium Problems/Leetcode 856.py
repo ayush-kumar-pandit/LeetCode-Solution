53e0bd26b83f4d6d1ae825db15ebef09283a02be
Ayush <ayushpandit672@gmail.com>
2026-04-21 20:23:20 +0530

    Scoring of Parentheses

---
+++ Medium Problems/Leetcode 856.py
@@ -0,0 +1,11 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        ans = depth = 0
+        for i in range(len(s)):
+            if s[i] == '(':
+                depth += 1
+            else:
+                depth -= 1
+                if s[i - 1] == '(':
+                    ans += 1 << depth
+        return ans
