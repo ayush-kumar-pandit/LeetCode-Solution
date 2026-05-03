214d2b5b9ebadd1763beb81c95c17ca86e4d5777
Ayush <ayushpandit672@gmail.com>
2026-05-03 15:59:59 +0530

    Optimized

--- Medium Problems/Leetcode 1855.py
+++ Medium Problems/Leetcode 1855.py
@@ -1,15 +1,9 @@
 class Solution:
     def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
-        res = 0
-        for i in range(len(nums1)):
-            l = i
-            h = len(nums2) - 1
-            while l <= h:
-                mid = l + (h - l) // 2
-                if nums2[mid] < nums1[i]:
-                    h = mid - 1
-                else:
-                    l = mid + 1
-            res = max(res, h - i)
-
-        return res
+        i = 0
+        for j in range(len(nums2)):
+            if nums1[i] > nums2[j]:
+                i += 1
+                if i == len(nums1):
+                    break
+        return max(j - i, 0)
