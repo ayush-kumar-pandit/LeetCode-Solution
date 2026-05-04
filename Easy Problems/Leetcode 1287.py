c938f8846a1a14e1216b6cda7ca2c5306b427d44
Ayush <ayushpandit672@gmail.com>
2026-05-05 00:44:45 +0530

    Optimized

--- Easy Problems/Leetcode 1287.py
+++ Easy Problems/Leetcode 1287.py
@@ -1,16 +1,7 @@
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
-        if len(arr) < 2:
-            return arr[0]
-        sz = len(arr) // 4
-        freq = {}
-        for i in arr:
-            if i not in freq:
-                freq[i] = 1
-            else:
-                freq[i] += 1
-        
-        maxx = max(freq.values())
-        for i in arr:
-            if freq[i] == maxx:
-                return i
+        x = len(arr)
+        y = x // 4
+        for i in range(x - y):
+            if arr[i] == arr[i + y]:
+                return arr[i]
