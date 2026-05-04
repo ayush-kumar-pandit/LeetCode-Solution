236930ef20fb1c37ba2194aa0c5d19bc0cf7ca47
Ayush <ayushpandit672@gmail.com>
2026-05-05 00:57:41 +0530

    Minimum Operations to Exceed Threshold Value I

---
+++ Easy Problems/Leetcode 3065.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        res = 0
+        for i in nums:
+            if i < k:
+                res += 1
+        return res
