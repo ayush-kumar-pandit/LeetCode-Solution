010017acf028b742c24f3322f29a1a2ef1ed2aef
Ayush <ayushpandit672@gmail.com>
2026-05-13 00:32:08 +0530

    Median of 2 Sorted Array (CPP Optimized)

--- Hard Problems/Leetcode 4.cpp
+++ Hard Problems/Leetcode 4.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        float ans;
         for(int i = 0; i < nums2.size(); i++){
             nums1.push_back(nums2[i]);
         }
