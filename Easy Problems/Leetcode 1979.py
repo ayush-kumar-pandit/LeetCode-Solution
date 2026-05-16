522c4f2693cc033c1175eca34d2d406e3d1b9e35
Ayush <ayushpandit672@gmail.com>
2026-05-16 06:14:06 +0530

    Find Greatest Common Divisor of Array

---
+++ Easy Problems/Leetcode 1979.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        mn = min(nums)
+        mx = max(nums)
+        for i in range(mn,0,-1):
+            if mx % i == 0 and mn % i == 0:
+                return i
