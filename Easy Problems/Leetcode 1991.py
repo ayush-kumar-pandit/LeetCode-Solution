55a7b3e33560a6a272877c7f7ef2ae5004db6c5c
Ayush <ayushpandit672@gmail.com>
2026-05-30 11:43:33 +0530

    Find the Middle Index in Array

---
+++ Easy Problems/Leetcode 1991.py
@@ -0,0 +1,10 @@
+total = sum(nums)
+        leftSum = 0
+
+        for i in range(len(nums)):
+            if leftSum == total - leftSum - nums[i]:
+                return i
+
+            leftSum += nums[i]
+
+        return -1
