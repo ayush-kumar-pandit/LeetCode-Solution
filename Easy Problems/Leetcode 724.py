547c90ca8282a44697f81daca7862dae8d4cf0f2
Ayush <ayushpandit672@gmail.com>
2026-05-30 11:41:59 +0530

    Find Pivot Index

---
+++ Easy Problems/Leetcode 724.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pivotIndex(self, nums):
+        total = sum(nums)
+        leftSum = 0
+
+        for i in range(len(nums)):
+            if leftSum == total - leftSum - nums[i]:
+                return i
+
+            leftSum += nums[i]
+
+        return -1
