28b54d4d15a7aa61ccb7587ef2ade99319f53146
Ayush <ayushpandit672@gmail.com>
2026-06-04 10:53:00 +0530

    Is Subsequence (CPP)

---
+++ Easy Problems/Leetcode 392.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0;
+        int j = 0;
+        int n = s.size();
+        int m = t.size();
+        while(i < n and j < m){
+            if(s[i] == t[j])
+                i++;
+            j++;
+        }
+        return i == s.size();
+    }
+};
