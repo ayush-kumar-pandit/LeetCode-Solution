0588434eafc54f57e0dbc4eecbe227d5834276ba
Ayush <ayushpandit672@gmail.com>
2026-06-06 22:19:27 +0530

    Find Smallest Letter Greater Than Target (Binary Search)

--- Easy Problems/Leetcode 744.py
+++ Easy Problems/Leetcode 744.py
@@ -2,6 +2,12 @@ class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
         if target == 'z' or ord(target) < ord(letters[0]) or ord(target) >= ord(letters[-1]):
             return letters[0]
-        for letter in letters:
-            if ord(target) < ord(letter):
-                return letter
+        l = 0
+        r = len(letters) - 1
+        while l < r:
+            mid = l + (r - l) // 2
+            if ord(letters[mid]) <= ord(target):
+                l = mid + 1
+            else:
+                r = mid
+        return letters[l]
