29f67e8090085523b29b66ca149274156c1d84ca
Ayush <ayushpandit672@gmail.com>
2026-06-10 07:33:02 +0530

    Maximum Total Subarray Value II

---
+++ Hard Problems/Leetcode 3691.py
@@ -0,0 +1,61 @@
+class Solution:
+    def maxTotalValue(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+
+
+        LOG = (n).bit_length()
+
+        st_max = [nums[:]]
+        st_min = [nums[:]]
+
+        j = 1
+        while (1 << j) <= n:
+            prev_max = st_max[j - 1]
+            prev_min = st_min[j - 1]
+            length = n - (1 << j) + 1
+
+            cur_max = [0] * length
+            cur_min = [0] * length
+
+            half = 1 << (j - 1)
+
+            for i in range(length):
+                cur_max[i] = max(prev_max[i], prev_max[i + half])
+                cur_min[i] = min(prev_min[i], prev_min[i + half])
+
+            st_max.append(cur_max)
+            st_min.append(cur_min)
+            j += 1
+
+        def value(l: int, r: int) -> int:
+            length = r - l + 1
+            p = length.bit_length() - 1
+
+            mx = max(
+                st_max[p][l],
+                st_max[p][r - (1 << p) + 1]
+            )
+            mn = min(
+                st_min[p][l],
+                st_min[p][r - (1 << p) + 1]
+            )
+
+            return mx - mn
+
+        heap = []
+
+        for l in range(n):
+            v = value(l, n - 1)
+            heappush(heap, (-v, l, n - 1))
+
+        ans = 0
+
+        for _ in range(k):
+            neg_v, l, r = heappop(heap)
+            ans += -neg_v
+
+            if r > l:
+                nv = value(l, r - 1)
+                heappush(heap, (-nv, l, r - 1))
+
+        return ans
