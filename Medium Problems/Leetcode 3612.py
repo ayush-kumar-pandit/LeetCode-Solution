d084d49fadf0846814c35eddb936e8ae426dc4eb
Ayush <ayushpandit672@gmail.com>
2026-06-16 22:42:41 +0530

    Process String with Special Operations I

---
+++ Medium Problems/Leetcode 3612.py
@@ -0,0 +1,14 @@
+class Solution:
+    def processStr(self, s: str) -> str:
+        res = []
+        for ch in s:
+            if ch.isalpha():
+                res.append(ch)
+            elif ch == "#":
+                res += res
+            elif ch == "%":
+                res.reverse()
+            else:
+                if res:
+                    res.pop()
+        return "".join(res)
