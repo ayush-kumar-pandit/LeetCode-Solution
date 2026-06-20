e10fc8bfce1fe76ebe991caed37c406744e31ae5
Ayush <ayushpandit672@gmail.com>
2026-06-20 17:19:51 +0530

    Summary Ranges

---
+++ Easy Problems/Leetcode 228.py
@@ -0,0 +1,20 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        res = []
+        n = len(nums)
+        i = 0
+
+        while i < n:
+            start = nums[i]
+
+            while i + 1 < n and nums[i] + 1 == nums[i + 1]:
+                i += 1
+
+            if start == nums[i]:
+                res.append(str(start))
+            else:
+                res.append(f"{start}->{nums[i]}")
+
+            i += 1
+
+        return res
