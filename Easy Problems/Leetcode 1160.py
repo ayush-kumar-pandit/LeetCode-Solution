a41e1a25d07fd8f974cf6823ee0c06a5cf7422c8
Ayush <ayushpandit672@gmail.com>
2026-06-22 15:56:01 +0530

    Find Words That Can Be Formed by Characters (Optimized)

--- Easy Problems/Leetcode 1160.py
+++ Easy Problems/Leetcode 1160.py
@@ -1,16 +1,9 @@
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
-        freq = {}
-        for ch in chars:
-            freq[ch] = freq.get(ch, 0) + 1
         res = 0
         for word in words:
-            freq_1 = {}
             for ch in word:
-                freq_1[ch] = freq_1.get(ch, 0) + 1
-
-            for ch in word:
-                if freq_1[ch] > freq.get(ch,0):
+                if word.count(ch) > chars.count(ch):
                     break
             else:
                 res += len(word)
