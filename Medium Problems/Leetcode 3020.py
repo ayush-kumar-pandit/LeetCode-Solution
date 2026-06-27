04fadf9f5b1fcc0331b2cc3d2ac8a662ee0087b3
Ayush <ayushpandit672@gmail.com>
2026-06-27 19:27:37 +0530

    Find the Maximum Number of Elements in Subset

---
+++ Medium Problems/Leetcode 3020.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        cnt = Counter(nums)
+
+        ans = cnt[1] if cnt[1] % 2 else cnt[1] - 1
+
+        for x in cnt:
+            if x == 1:
+                continue
+
+            cur = x
+            length = 0
+
+            while cnt[cur] >= 2:
+                length += 2
+                cur *= cur
+
+            if cnt[cur] == 1:
+                length += 1
+            else:
+                length -= 1
+
+            ans = max(ans, length)
+
+        return ans
