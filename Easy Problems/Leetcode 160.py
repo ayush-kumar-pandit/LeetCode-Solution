9127cf9eb3a4188ef7a1c473610e63492b1d25cc
Ayush <ayushpandit672@gmail.com>
2026-07-16 15:22:46 +0530

    Intersection of Two Linked Lists

---
+++ Easy Problems/Leetcode 160.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        s = set()
+        temp = headA
+        while temp:
+            s.add(temp)
+            temp = temp.next
+        
+        temp = headB
+        while temp:
+            if temp in s:
+                return temp
+            temp = temp.next
+        return None
