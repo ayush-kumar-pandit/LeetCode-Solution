4125555f7e947baea712edc19e21cddbd357e2ac
Ayush <ayushpandit672@gmail.com>
2026-07-28 22:20:52 +0530

    Masking Personal Information

---
+++ Medium Problems/Leetcode 831.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def maskPII(self, s: str) -> str:
+
+        at = s.find('@')
+        if at >= 0:
+            return (s[0] + "*" * 5 + s[at - 1:]).lower()
+        s = "".join(i for i in s if i.isdigit())
+        return ["", "+*-", "+**-", "+***-"][len(s) - 10] + "***-***-" + s[-4:]
