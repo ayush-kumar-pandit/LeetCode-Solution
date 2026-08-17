fe597128db4700807a8501cb549161bf922cf8e5
Ayush <ayushpandit672@gmail.com>
2026-08-17 22:32:36 +0530

    Jump Game

---
+++ Medium Problems/Leetcode 55.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        mx = 0
+        for i in range(len(nums)):
+            if i > mx:
+                return False
+            mx = max(mx, i + nums[i])
+        return True
